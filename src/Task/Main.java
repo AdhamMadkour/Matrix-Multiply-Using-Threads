31ff0cedc6bca688b0e84030b7b5db80b044c67a
AdhamMadkour <64507869+AdhamMadkour@users.noreply.github.com>
2021-12-17 11:10:57 +0200

    Used countDownLatch

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -1,6 +1,7 @@
 package Task;
 
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
@@ -9,15 +10,15 @@ public class Main {
         return m == x;
     }
 
-    public static int[][] mult(int[][] M1, int[][] M2, int n, int m, int y) {
+    public static int[][] mult(int[][] M1, int[][] M2, int n, int m, int y) throws InterruptedException {
         int[][] Ans = new int[n][y];
+        CountDownLatch startSignal = new CountDownLatch(1);// Start signal that prevents any thread from proceeding until the driver is ready for them to proceed
+        CountDownLatch countDownLatch = new CountDownLatch(n);//wait until all threads have completed
         for (int i = 0; i < n; ++i) {       // for first matrix row
-            for (int j = 0; j < y; ++j) {  //for second and first matrix column and row
-                // 3 * 3 matrix by 3 * 2 matrix will make 3 * 2 = 6 thread at the same time
-                ThreadCalc threadCalc = new ThreadCalc(M1, M2, Ans, i, j, m);
-                threadCalc.start();
-            }
+            new Thread(new ThreadCalc(startSignal, countDownLatch, M1, M2, Ans, i, y, m)).start();
         }
+        startSignal.countDown();
+        countDownLatch.await();
         return Ans;
     }
 
@@ -31,18 +32,18 @@ public class Main {
         return Matrix;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int m1Row, m1Col, m2Row, m2Col;
         m1Row = scanner.nextInt();
         m1Col = scanner.nextInt();
-        int[][] M1 = new int[m1Row][m1Col];
+        int[][] M1;
         M1 = inMatrix(m1Row, m1Col);
         m2Row = scanner.nextInt();
         m2Col = scanner.nextInt();
-        int[][] M2 = new int[m2Row][m2Col];
+        int[][] M2;
         M2 = inMatrix(m2Row, m2Col);
         if (check(m1Row, m1Col, m2Row, m2Col)) {
-            int[][] Ans = new int[m1Row][m2Col];
+            int[][] Ans;
             Ans = mult(M1, M2, m1Row, m1Col, m2Col);
             for (int i = 0; i < m1Row; ++i) {
                 for (int j = 0; j < m2Col; ++j) {
--- src/Task/ThreadCalc.java
+++ src/Task/ThreadCalc.java
@@ -1,14 +1,41 @@
 package Task;
 
-public class ThreadCalc extends Thread {
-    public ThreadCalc(int[][] M1, int[][] M2, int[][] Ans, int i, int j, int m) {
-        // Constructor that will calculate single cell in the matrix
-        for (int k = 0; k < m; ++k) {
-            Ans[i][j] += M1[i][k] * M2[k][j];
-        }
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class ThreadCalc implements Runnable {
+    private static int[][] M1;
+    private static int[][] M2;
+    private static int[][] Ans;
+    private static int y, m;
+    private final int i; //Cannot be passed by reference
+    private final CountDownLatch latch;
+    private final CountDownLatch startSignal;
+
+    public ThreadCalc(CountDownLatch startSignal, CountDownLatch latch, int[][] M1, int[][] M2, int[][] Ans, int i, int y, int m) {
+        // Constructor that will calculate single row in the matrix
+        ThreadCalc.M1 = M1;
+        ThreadCalc.M2 = M2;
+        ThreadCalc.Ans = Ans;
+        ThreadCalc.y = y;
+        ThreadCalc.m = m;
+        this.i = i;
+        this.startSignal = startSignal;
+        this.latch = latch;
     }
 
     @Override
     public void run() {
+        try {
+            startSignal.await();
+            for (int j = 0; j < y; ++j) {
+                for (int k = 0; k < m; ++k) {
+                    Ans[i][j] += M1[i][k] * M2[k][j];
+                }
+            }
+            latch.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
